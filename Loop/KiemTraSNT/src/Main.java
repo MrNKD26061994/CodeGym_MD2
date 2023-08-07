6c341bcda520c60d5d639457b93c035f875ab974
unknown <elearning.nkd@gmail.com>
2023-08-07 08:50:12 +0700

    Thực hành KT SNT

---
+++ Loop/KiemTraSNT/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if(number < 2){
+            System.out.println(number + " is not a prime");
+        } else {
+            boolean flag = true;
+            for (int i = 2; i < Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                System.out.println(number + " is a prime");
+            } else {
+                System.out.println(number + " is not a prime");
+            }
+        }
+    }
+}
