58fde45074b5fdca9f7582e0325eab22b5e968a3
unknown <elearning.nkd@gmail.com>
2023-08-07 11:06:15 +0700

    Update sử dụng hàm thành switch

--- Introduction/DocSoThanhChu/src/Main.java
+++ Introduction/DocSoThanhChu/src/Main.java
@@ -8,179 +8,71 @@ public class Main {
         number = scanner.nextInt();
         int hundreds = number / 100;
         int tens = (number - hundreds * 100) / 10;
+        int ten = number - hundreds * 100;
         int ones = number - hundreds * 100 - tens * 10;
+
+        String str1 = switch (hundreds) {
+            case 1 -> "One hundred";
+            case 2 -> "Two hundred";
+            case 3 -> "Three hundred";
+            case 4 -> "Four hundred";
+            case 5 -> "Five hundred";
+            case 6 -> "Six hundred";
+            case 7 -> "Seven hundred";
+            case 8 -> "Eight hundred";
+            case 9 -> "Nine hundred";
+            default -> "";
+        };
+
+        String str2 = switch (tens) {
+            case 1 -> {
+                String res = "";
+                switch (ten) {
+                    case 10 -> res = "ten";
+                    case 11 -> res = "eleven";
+                    case 12 -> res = "twelve";
+                    case 13 -> res = "thirteen";
+                    case 14 -> res = "fourteen";
+                    case 15 -> res = "fifteen";
+                    case 16 -> res = "sixteen";
+                    case 17 -> res = "seventeen";
+                    case 18 -> res = "eighteen";
+                    case 19 -> res = "nineteen";
+                }
+                yield res;
+            }
+            case 2 -> "twenty ";
+            case 3 -> "thirty ";
+            case 4 -> "forty ";
+            case 5 -> "fifty ";
+            case 6 -> "sixty ";
+            case 7 -> "seventy ";
+            case 8 -> "eighty ";
+            case 9 -> "ninety ";
+            default -> "";
+        };
+
+        String str3 = switch (ones) {
+            case 1 -> "one";
+            case 2 -> "two";
+            case 3 -> "three";
+            case 4 -> "four";
+            case 5 -> "five";
+            case 6 -> "six";
+            case 7 -> "seven";
+            case 8 -> "eight";
+            case 9 -> "nine";
+            default -> "";
+        };
+
         String result = "";
-        if(number > 999 || number <= 0) {
-            result += "Số nhâp phải lớn hơn 0 hoặc nhỏ hơn 999";
-        } else {
-            if(hundreds != 0) {
-                result += stringHundred(hundreds);
-                if(tens != 0) {
-                    result += " and ";
-                    if(tens == 1) {
-                        result += stringTeen(number - hundreds * 100);
-                    } else {
-                        result += stringTens(tens) + " " + stringOnes(ones);
-                    }
-                } else {
-                    if(ones != 0) {
-                        result += " and ";
-                        result += stringOnes(ones);
-                    }
-                }
-            } else {
-                if(tens != 0) {
-                    if(tens == 1) {
-                        result += stringTeen(number - hundreds * 100);
-                    } else {
-                        result += stringTens(tens) + " " + stringOnes(ones);
-                    }
-                } else {
-                    if(ones != 0) {
-                        result += stringOnes(ones);
-                    }
-                }
-            }
+        if(tens == 1){
+            str3 = "";
         }
-
+        if(!str1.isEmpty() && ten != 0) {
+            result = str1 + " and ";
+        }
+        result += str2 + str3;
         System.out.println(result);
-    }
-    public static String stringOnes(int ones) {
-        switch (ones) {
-            case 0 -> {
-                return "";
-            }
-            case 1 -> {
-                return "one";
-            }
-            case 2 -> {
-                return "two";
-            }
-            case 3 -> {
-                return "three";
-            }
-            case 4 -> {
-                return "four";
-            }
-            case 5 -> {
-                return "five";
-            }
-            case 6 -> {
-                return "six";
-            }
-            case 7 -> {
-                return "seven";
-            }
-            case 8 -> {
-                return "eight";
-            }
-            case 9 -> {
-                return "nine";
-            }
-            default -> {
-                return "Out of ability";
-            }
-        }
-    }
-
-    public static String stringTeen(int teen) {
-        switch (teen) {
-            case 10 -> {
-                return "ten";
-            }
-            case 11 -> {
-                return "eleven";
-            }
-            case 12 -> {
-                return "twelve";
-            }
-            case 13 -> {
-                return "thirteen";
-            }
-            case 14 -> {
-                return "fourteen";
-            }
-            case 15 -> {
-                return "fifteen";
-            }
-            case 16 -> {
-                return "sixteen";
-            }
-            case 17 -> {
-                return "seventeen";
-            }
-            case 18 -> {
-                return "eighteen";
-            }
-            case 19 -> {
-                return "nineteen";
-            }
-            default -> {
-                return "Out of ability";
-            }
-        }
-    }
-
-    public static String stringTens(int tens) {
-        switch (tens) {
-            case 2 -> {
-                return "twenty";
-            }
-            case 3 -> {
-                return "thirty";
-            }
-            case 4 -> {
-                return "forty";
-            }
-            case 5 -> {
-                return "fifty";
-            }
-            case 6 -> {
-                return "sixty";
-            }
-            case 7 -> {
-                return "seventy";
-            }
-            case 8 -> {
-                return "eighty";
-            }
-            case 9 -> {
-                return "ninety";
-            }
-        }
-        return "";
-    }
-
-    public static String stringHundred(int hundred) {
-        switch (hundred) {
-            case 1 -> {
-                return "One hundred";
-            }
-            case 2 -> {
-                return "Two hundred";
-            }
-            case 3 -> {
-                return "Three hundred";
-            }
-            case 4 -> {
-                return "Four hundred";
-            }
-            case 5 -> {
-                return "Five hundred";
-            }
-            case 6 -> {
-                return "Six hundred";
-            }
-            case 7 -> {
-                return "Seven hundred";
-            }
-            case 8 -> {
-                return "Eight hundred";
-            }
-            case 9 -> {
-                return "Nine hundred";
-            }
-        }
-        return "";
     }
 }
