51017ec4140f5948fe34ce37bcf9304e9efd2e98
unknown <elearning.nkd@gmail.com>
2023-08-09 10:01:25 +0700

    Bài tập Fan

---
+++ Class-Object/Fan/src/Fan.java
@@ -0,0 +1,69 @@
+public class Fan {
+    final private int SLOW = 1;
+    final private int MEDIUM = 2;
+    final private int FAST = 3;
+
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public Fan() {
+        this.speed = this.SLOW;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+    }
+
+    public String toString() {
+        if(this.on) {
+            return "fan is on speed = " + this.speed + ", radius = " + this.radius + ", color = " + this.color;
+        } else {
+            return "fan is off";
+        }
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSLOW() {
+        return SLOW;
+    }
+
+    public int getMEDIUM() {
+        return MEDIUM;
+    }
+
+    public int getFAST() {
+        return FAST;
+    }
+}
