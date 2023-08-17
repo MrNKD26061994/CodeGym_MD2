9e018d4eb9bd33f750853839351a35374c355301
DuyNguyen <elearning.nkd@gmail.com>
2023-08-17 23:38:56 +0700

    test

---
+++ DSA-List/MyLinkedList/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(10);
+        myLinkedList.add(101);
+//        myLinkedList.add(1,10);
+        System.out.println(myLinkedList.get(1));
+    }
+}
--- DSA-List/MyLinkedList/src/MyLinkedList.java
+++ DSA-List/MyLinkedList/src/MyLinkedList.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class MyLinkedList {
+public class MyLinkedList {
+
+    private Node head;
+    private int numNodes;
+
+    public MyLinkedList() {}
+
+    public MyLinkedList(Object data) {
+        head = new Node(data);
+    }
+
+    public void add(int index, Object data) {
+        Node temp = head;
+        Node holder;
+        for (int i = 0; i < index - 1 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+    }
+
+    public void add(Object data) {
+        Node temp = head;
+        while (temp.next == null) {
+            temp = temp.next;
+        }
+        Node holder = temp.next;
+        temp.next = new Node(data);
+        temp.next.next = holder;
+        numNodes++;
+    }
+
+    public Node get(int index) {
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    private class Node{
+        private Node next;
+        private final Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+        public Object getData() {
+            return this.data;
+        }
+    }
 }
